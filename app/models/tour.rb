bc86281fdbe465c88e430c3fefd32ee1a8413e04
Ethan Hayon <ehayon@gmail.com>
2013-10-29 22:26:06 -0400

    validation on tour

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,20 +1,13 @@
 class Tour < ActiveRecord::Base 
-  
+
   # associations
-  has_one :creator, :class_name => 'User' 
+  belongs_to :creator, :class_name => 'User' 
   has_one :editor, :class_name => 'User'
 
-
-  
   # validators
   validates :name, :presence => true
   validates :description, :presence => true
+  validates :lat, :numericality => { :greater_than => -90.0, :less_than => 90.0 }
+  validates :lon, :numericality => { :greater_than => -180.0, :less_than => 180.0 }
 
-  validates :lat, :format => { :with => /\A\d{3}\.\d{6}\z/ },
-                  :numericality => { :greater_than => -90.0, :less_than => 90.0 }
-
-  validates :lon, :format => { :with => /\A\d{3}\.\d{6}\z/ },
-                  :numericality => { :greater_than => -180.0, :less_than => 180.0 }
-
-  
 end
