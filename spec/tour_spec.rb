98bfa34f9471b44934c22da115a12812c3c07234
Ethan Hayon <ehayon@gmail.com>
2013-10-30 20:40:02 -0400

    add some tests for lat lon validation

--- spec/tour_spec.rb
+++ spec/tour_spec.rb
@@ -7,4 +7,28 @@ describe 'Tours' do
     tour.should_not be_valid
   end
 
+  it 'rejects latitudes less than -90.0' do
+    tour = FactoryGirl.build(:tour, :lat => -100.000000)
+    tour.should_not be_valid
+  end
+
+  it 'accepts latitudes greater than -90 and less than 90' do
+    tour = FactoryGirl.build(:tour, :lat => 10.989898)
+    tour.should be_valid
+  end
+
+  it 'rejects longitudes greater than 180.0' do
+    tour = FactoryGirl.build(:tour, :lon => 200.092839)
+    tour.should_not be_valid
+  end
+
+  it 'rejects longitudes less than -180.0' do
+    tour = FactoryGirl.build(:tour, :lon => -200.000000)
+    tour.should_not be_valid
+  end
+
+  it 'accepts longitudes greater than -180 and less than 180' do
+    tour = FactoryGirl.build(:tour, :lat => 10.989898)
+    tour.should be_valid
+  end
 end
