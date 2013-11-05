5f7ac6cf5380dd5ffa37b53922ad85138fc1cddb
Chris Serenita <cserenit@stevens.edu>
2013-11-05 15:44:13 -0500

    Added stop table to rake migration

---
+++ db/migrate/20131105202558_create_stop_table.rb
@@ -0,0 +1,21 @@
+class CreateStopTable < ActiveRecord::Migration
+  def up
+    create_table :stops do |t|
+      t.string :name
+      t.text :description
+      t.integer :category_id
+      t.references :editor
+      t.references :creator
+      t.boolean :visibility
+      t.decimal :lat, :scale => 6, :precision => 9
+      t.decimal :lon, :scale => 6, :precision =>9
+      t.integer :parent_id
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :stops
+  end
+end
