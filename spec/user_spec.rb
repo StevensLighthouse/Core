430f152bf79e734925e1f70da78c2299da45a8ac
Ethan Hayon <ehayon@gmail.com>
2013-11-27 17:12:02 -0500

    some tests for user model

---
+++ spec/user_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'Users' do
+
+  it 'rejects mismatching password confirmation' do
+    user = FactoryGirl.build(:user, :password_confirmation => '')
+    user.should_not be_valid
+  end
+
+  it 'accepts user with matching password and password confirmation' do
+    user = FactoryGirl.build(:user)
+    user.should be_valid
+  end
+
+  it 'rejects user with no password confirmation' do
+    user = FactoryGirl.build(:user, :password_confirmation => '')
+    user.should_not be_valid
+  end
+
+  it 'rejects user without email address' do
+    user = FactoryGirl.build(:user, :email => '')
+    user.should_not be_valid
+  end
+
+  it 'rejects user with invalid email address' do
+    user = FactoryGirl.build(:user, :email => 'bademail')
+    user.should_not be_valid
+
+    user = FactoryGirl.build(:user, :email => 'a@b.c')
+    user.should_not be_valid
+  end
+
+end
