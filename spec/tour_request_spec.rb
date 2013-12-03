b1eb0b3b0abf7a3911ecb5720af8bf3eb9bed843
Ethan Hayon <ehayon@gmail.com>
2013-12-03 17:57:09 -0500

    structure for tour api specs

---
+++ spec/tour_request_spec.rb
@@ -0,0 +1,16 @@
+describe "Tour API" do
+  
+  it 'creates a tour' do
+    
+  end
+
+  it 'returns tour information' do
+
+  end
+
+  it 'lists all tours within boundary' do
+    # boundary = [ lat , lon ]
+
+  end
+
+end
