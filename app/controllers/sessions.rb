779dea80b56c251f989f115d4012af23d5c43f80
Ethan Hayon <ehayon@gmail.com>
2013-12-03 19:13:36 -0500

    session log in

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,3 +1,5 @@
+enable :sessions
+
 # GET /login
 # Provides a login page
 get '/login' do 
@@ -18,12 +20,20 @@ post '/login' do
     @session = Session.new(:ip_address => request.ip, :user_agent => request.user_agent, :expiry_time => 24.hours.from_now) 
     if @session.save
       # user is logged in
+      session[:user_id] = user.id
+      respond_to do |wants|
+        wants.html { "Log out" }
+      end
     else
       # unexpected error occured
+      respond_to do |wants|
+        wants.html { "Unexpected Error" }
+      end
     end
   else
     res = { :response_code => 400, :response => 'bad login' }
     respond_to do |wants|
+      wants.html { "Bad login" }
       wants.json { res.to_json }
     end
   end
