7716950abc0b122fc3d5cc41e5bc035dc542edcf
Phil Barresi <phil@ecpbstudios.com>
2014-01-30 14:31:47 -0500

    Split angular js for clarity

---
+++ public/js/controllers.js
@@ -0,0 +1,75 @@
+var coreControllers = angular.module('coreControllers', []);
+
+coreControllers.controller('HomeCtrl',
+    function ($scope, $dataService) {
+        $dataService.getAllData();
+    });
+
+coreControllers.controller('TourCtrl',
+    function ($scope, $dataService) {
+        $dataService.getAllTours(function (tours) {
+            $scope.tours = tours;
+        });
+    });
+
+coreControllers.controller('TourDetailCtrl',
+    function ($scope, $routeParams, $dataService) {
+        $scope.tourId = $routeParams.tourId;
+
+        $scope.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 5
+        };
+
+        $scope.setCenter = function (lat, lng) {
+            $scope.map.center.latitude = lat;
+            $scope.map.center.longitude = lng;
+        }
+
+        $scope.centerOn = function (stop) {
+            $scope.setCenter(stop.lat, stop.lon);
+        };
+
+        $dataService.getTour($scope.tourId, function (tour) {
+            $scope.name = tour.name;
+            $scope.stops = tour.stops;
+            $scope.description = tour.description;
+            $scope.map.center.latitude = parseFloat(tour.lat);
+            $scope.map.center.longitude = parseFloat(tour.lon);
+            $scope.map.zoom = 15;
+        });
+    });
+
+coreControllers.controller('GroupDetailCtrl',
+    function ($scope, $routeParams, $dataService) {
+        $scope.groupId = $routeParams.groupId;
+
+        $dataService.getGroup($scope.groupId, function (group) {
+            $scope.name = group.name;
+            $scope.description = group.description;
+        });
+    });
+
+coreControllers.controller('GroupCtrl',
+    function ($scope, $dataService) {
+        $dataService.getAllGroups(function (groups) {
+            $scope.groups = groups;
+        });
+    });
+
+coreControllers.controller('UserCtrl',
+    function ($scope, $dataService) {
+        $dataService.getAllUsers(function (users) {
+            $scope.users = users;
+        });
+    });
+
+coreControllers.controller('StopCtrl',
+    function ($scope, $dataService) {
+        $dataService.getAllStops(function (stops) {
+            $scope.stops = stops;
+        });
+    });
