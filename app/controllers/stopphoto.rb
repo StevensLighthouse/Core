58f199e047e45587f01fc08e63b726de0e0ae5df
Ethan Hayon <ehayon@gmail.com>
2014-02-28 21:33:04 -0500

    set up controller for photo

---
+++ app/controllers/stopphoto.rb
@@ -0,0 +1,17 @@
+# POST: Upload a new photo
+post 'photos/upload' do
+
+end
+
+# PUT: Update data for a photo
+put 'photos/:id' do
+
+end
+
+# DELETE: Destroy this photo, delete
+# associated data
+delete 'photos/:id' do
+
+end
+
+
